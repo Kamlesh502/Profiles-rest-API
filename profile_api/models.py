b1fdd6975b1fd41e26f81069cd2b4520645a36c4
kamlesh502 <kamleshbishnoi502@gmail.com>
2020-09-24 02:05:17 +0530

    Solved errors regrading super user..

--- profile_api/models.py
+++ profile_api/models.py
@@ -10,16 +10,22 @@ class UserProfileManager(BaseUserManager):
         """Create a new user profile""" 
         if not email:
             raise ValueError('User must have abn email add')
+
         email=self.normalize_email(email)
-        user =self.model(email=email,name=name)
+        user =self.model(email=email,name=name,)
+
+        
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+        
     def create_superuser(self,email,name,password):
         """Create and save a new superuser with given details"""
-        user =self.create_superuser(email,name,password)
+        user =self.create_user(email,name,password)
 
+        
         user.is_superuser=True
         user.is_staff=True
         user.save(using=self._db)
@@ -28,21 +34,22 @@ class UserProfileManager(BaseUserManager):
 
     
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for user in the system"""
-    email=models.EmailField(max_length=255,unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)  
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects =UserProfileManager()
 
     USERNAME_FIELD="email"
-    REQUIRED_FIELD=['name']
+    REQUIRED_FIELDS=['name']
 
     def get_full_name(self):
         """Retrive full name of user"""
         return self.name
+
     def get_short_name(self):
         """Retrive short name"""
         return self.name
